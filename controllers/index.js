1d9ace6160a5b8eb2bb87dfa43cac70981d4eec4
PintoDrop <mikebender22@gmail.com>
2023-04-13 09:23:36 -0700

    working on routes

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+const userRoutes = require ("./users");
+const thoughtRoutes = require("./thoughts");
+
+router.use("/api", apiRoutes);
+router.use("/", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+
+module.exports = router;
